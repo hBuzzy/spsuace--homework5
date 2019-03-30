5d585ef788b12547b7efd74001b1af06470d0ea7
alexander.galkin <alexander.galkin@corp.mail.ru>
2019-03-30 11:28:49 +0300

    added test and changed description

---
+++ src/test/java/ru/spsuace/homework5/streams/WordFrequencyTest.java
@@ -0,0 +1,34 @@
+package ru.spsuace.homework5.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordFrequencyTest {
+
+    @Test
+    public void simple() {
+        Stream<String> innerStream = Arrays.stream(new String[]{"Мама мыла мыла мыла раму!"});
+        List<String> expected = Arrays.asList("мыла", "маму", "раму!");
+        List<String> actual = WordFrequency.wordFrequency(innerStream);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void standard() {
+        Stream<String> innerStream = Arrays.stream(new String[]{
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus.",
+                "Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt",
+                "efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac",
+                "auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi",
+                "tempor dignissim"});
+        List<String> expected = Arrays.asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet",
+                "dolor", "eget", "elit", "mi");
+        List<String> actual = WordFrequency.wordFrequency(innerStream);
+        assertEquals(expected, actual);
+    }
+}
