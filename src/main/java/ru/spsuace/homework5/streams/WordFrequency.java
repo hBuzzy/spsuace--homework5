b33019f37afcf3703691a688ccc625c53cabf493
hBuzzy <nikbalsea@gmail.com>
2019-03-31 05:35:43 +0300

    Frequency v1.2

--- src/main/java/ru/spsuace/homework5/streams/WordFrequency.java
+++ src/main/java/ru/spsuace/homework5/streams/WordFrequency.java
@@ -1,6 +1,10 @@
 package ru.spsuace.homework5.streams;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +25,17 @@ public class WordFrequency {
      * Без них - 4 балла
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+        return lines.map(w -> w.split("\\s+"))
+                .flatMap(Arrays::stream)
+                .map(String::toLowerCase)
+                .map(w -> w.replaceAll("[.,]", ""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
 
